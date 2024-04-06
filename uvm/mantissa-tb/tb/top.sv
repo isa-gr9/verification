6f553957501d9ed12f16c0e084f1ac5edbdcf38c
Riccardo Fusari <ric.fusari@gmail.com>
2024-04-06 22:11:32 +0200

    Automated sync:  M es4part2/tb/refmod.sv  D uvm/mantissa-tb/tb/hw_top.sv  D uvm/mantissa-tb/tb/mul_if.sv  D uvm/mantissa-tb/tb/mul_pkg.sv  M uvm/mantissa-tb/tb/refmod.sv  M uvm/mantissa-tb/tb/top.sv ?? uvm/fpu-tb/.vscode/ ?? uvm/fpu-tb/c/ ?? uvm/mantissa-tb/src/DUT.sv ?? uvm/mantissa-tb/src/dut_if.sv

--- uvm/mantissa-tb/tb/top.sv
+++ uvm/mantissa-tb/tb/top.sv
@@ -1,9 +1,52 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "../src/CS_block.sv"
+`include "../src/CSA.sv"
+`include "../src/daddatree.sv"
+`include "../src/fullAdder.sv"
+`include "../src/halfAdder.sv"
+`include "../src/LUT.sv"
+`include "../src/RCA.sv"
+`include "../src/multiplier.sv"
+`include "../src/dut_if.sv"
+`include "../src/DUT.sv"
+
+
+`include "../tb/packet_in.sv"
+`include "../tb/packet_out.sv"
+`include "../tb/sequence_in.sv"
+`include "../tb/sequencer.sv"
+`include "../tb/driver.sv"
+`include "../tb/driver_out.sv"
+`include "../tb/monitor.sv"
+`include "../tb/monitor_out.sv"
+`include "../tb/agent.sv"
+`include "../tb/agent_out.sv"
+`include "../tb/refmod.sv"
+`include "../tb/comparator.sv"
+`include "../tb/env.sv"
+`include "../tb/simple_test.sv"
+
+//Top
 module top;
-
-  import uvm_pkg::*;
-  `include "uvm_macros.svh"
-
-  import mul_pkg::*;
+  logic clk;
+  logic rst;
+  
+  initial begin
+    clk = 0;
+    rst = 1;
+    #22 rst = 0;
+    
+  end
+  
+  always #5 clk = !clk;
+  
+  logic [1:0] state;
+  
+  dut_if in(clk, rst);
+  dut_if out(clk, rst);
+  
+  DUT sum(in, out, state);
 
   initial begin
     `ifdef INCA
@@ -17,10 +60,8 @@ module top;
       set_config_int("*", "recording_detail", 1);
     `endif
 
-    // uvm_config_db#(input_vif)::set(uvm_root::get(), "*.env_h.mst.*", "vif", in);
-    // uvm_config_db#(output_vif)::set(uvm_root::get(), "*.env_h.slv.*",  "vif", out);
-
-    vif_config::set(null, "*.env_h.mst.*", "vif", hw_top.intf);
+    uvm_config_db#(input_vif)::set(uvm_root::get(), "*.env_h.mst.*", "vif", in);
+    uvm_config_db#(output_vif)::set(uvm_root::get(), "*.env_h.slv.*",  "vif", out);
 
     run_test("simple_test");
   end
